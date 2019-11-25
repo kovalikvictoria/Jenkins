0fd4dfad137d6d8ba6741ed9d9bf31a20a2382ae
Vika Kovalik <vkovalik583@gmail.com>
2019-11-25 10:58:52 +0200

    First commit

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jenkins
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Start test");
+            int i = 0;
+            i = 1 / i;
+            Console.WriteLine("Test done");
+        }
+    }
+}
